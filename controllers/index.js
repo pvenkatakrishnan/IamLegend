069d12bf0f344f02e877c2ca9cb4db54148cf4de
pvenkatakrishnan <pvenkatakrishnan@paypal.com>
2013-12-30 14:51:27 -0800

    changing the sample app to use the changes in karka and adaro to support specialization

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,9 @@ module.exports = function (server) {
 
     server.get('/', function (req, res) {
         var model = { name: 'Iamlegend', engine: 'dustjs', stack: 'krakenjs' };
-        
+        model.experiments = {
+			foo: true
+        };
         res.render('index', model);
         
     });
