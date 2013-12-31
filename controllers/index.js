23a682823e7e0ca4161112f6ba3416e14a90bf26
pvenkatakrishnan <pvenkatakrishnan@paypal.com>
2013-12-31 14:26:49 -0800

    Fixing some comments

--- controllers/index.js
+++ controllers/index.js
@@ -8,19 +8,22 @@ module.exports = function (server) {
         
         //Test 1: for experiements specialization
 
-        /*res.locals({
+        res.locals({
             experiments : {
                 foo: true
             }
-        });*/
-
-        //Test 2:  for locales specialization
-        
-        res.locals({
-            locality: 'es-US'
         });
 
-        //TODO : what if we need both experiemnt + locale ?
+        //Test 2:  for locales specialization
+        //this is not the correct way of setting locales. Locale needs to be set
+        //outside local context. But this works for a test 
+        //TODO to figure out the right way to set the locale.
+        
+        /*res.locals({
+            locality: 'es-US'
+        });*/
+    
+        //TODO : what if we need both experiemnt + locale  specialization?
 
 
         res.render('index', model);
