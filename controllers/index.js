34ec20d341850ccb206b6b82cde1fec6cb694c26
pvenkatakrishnan <pvenkatakrishnan@paypal.com>
2013-12-31 11:26:29 -0800

    Some cleanup

--- controllers/index.js
+++ controllers/index.js
@@ -7,23 +7,18 @@ module.exports = function (server) {
         var model = { name: 'Iamlegend', engine: 'dustjs', stack: 'krakenjs' };
         
         //Test 1: for experiements specialization
-        //just a sample to try if experiments work
-        //obviously the experiments wont be specified here in the model
 
-        /*model.experiments = {
-			foo: true
-        };*/
-        res.locals({
+        /*res.locals({
             experiments : {
                 foo: true
             }
-        });
+        });*/
 
         //Test 2:  for locales specialization
         
-        /*res.locals({
+        res.locals({
             locality: 'es-US'
-        });*/
+        });
 
         //TODO : what if we need both experiemnt + locale ?
 
