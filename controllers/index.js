d11d576f70346c388d7d7293f79275a75d11a1e8
pvenkatakrishnan <pvenkatakrishnan@paypal.com>
2013-12-30 18:35:37 -0800

    Adding test for both localization as well as for experiment

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,26 @@ module.exports = function (server) {
 
     server.get('/', function (req, res) {
         var model = { name: 'Iamlegend', engine: 'dustjs', stack: 'krakenjs' };
-        model.experiments = {
+        
+        //Test for experiements specialization
+        //just a sample to try if experiments work
+        //obviously the experiments wont be specified here in the model
+
+        /*model.experiments = {
 			foo: true
-        };
+        };*/
+
+        //Test for locales specialization
+        //try to override the locales to check if the right templates get loaded
+        console.info('res.locals:' + res.locals);
+
+        res.locals({
+            locality: 'es-US'
+        });
+
+        //TODO : what if we need both experiemnt + locale ?
+
+
         res.render('index', model);
         
     });
