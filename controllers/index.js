9b325b45598ff5bf1610a1d47c3c9195598a52ed
pvenkatakrishnan <pvenkatakrishnan@paypal.com>
2014-01-07 12:25:09 -0800

    Adding some minor things that i need to figure out where to add to in the framework

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,21 @@
 'use strict';
+var nconf = require('nconf');
 
 module.exports = function (server) {
 
+
     server.get('/', function (req, res) {
         var model = { name: 'Iamlegend', engine: 'dustjs', stack: 'krakenjs' };
+        
+        //TODO IMPORTANT!!!!!!
+        //this should be figured out in the framework somewhere
+        res.locals({
+            templatePath: (process.env.NODE_ENV === 'production') ? '/IamLegend/templates/US/en' : '/IamLegend/templates'
+        });
+        res.locals({
+            _specialization: nconf.get('specialization')
+        });
+
         //Test 1: for experiements specialization
 
         res.locals({
