9881446bd3f523923f47ad4a51ec19e88eb6af67
poornima <pvenkatakrishnan@paypal.com>
2014-03-06 18:36:26 -0800

    Modifying the app to be able to render for both js and dust engines

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ var express = require('express'),
     kraken = require('kraken.next'),
     options = {
         onconfig: function (settings, cb) {
-            console.info('****s', settings.get('express:views'));
             var config = {
-                views: settings.get('express:views'),
-                i18n: settings.get('i18n'),
-                specialization: settings.get('specialization')
+                'views': settings.get('express:views'),
+                'view engine': settings.get('express:view engine'),
+                'i18n': settings.get('i18n'),
+                'specialization': settings.get('specialization')
             };
             var engines = settings.get('view engines');
-            engines.js.renderer['arguments'] = [config, app];
-            engines.dust.renderer['arguments'] = [config];
-            console.info(engines);
+
+            engines.dust.renderer['arguments'].push(config);
+            engines.js.renderer['arguments'].push(config, app);
+
             settings.set('view engines', engines);
             cb(null, settings);
         }
@@ -23,45 +24,3 @@ var express = require('express'),
 
 app.use(kraken(options));
 app.listen(8000);
-/*app.configure = function configure(nconf, next) {
-    // Fired when an app configures itself
-    next();
-};
-
-
-app.requestStart = function requestStart(server) {
-    // Fired at the beginning of an incoming request
-};
-
-
-app.requestBeforeRoute = function requestBeforeRoute(server) {
-    // Setting all the res.locals information for the
-    // specialization to happen
-    server.use(function(req, res, next) {
-        res.locals({
-            templatePath: (process.env.NODE_ENV === 'production') ? '/IamLegend/templates/US/en' : '/IamLegend/templates'
-        });
-        res.locals({
-            context: {
-                locality: 'es_US'
-            }
-        });
-        res.locals({
-            device: 'tablet'
-        });
-        next();
-    });
-};
-
-
-app.requestAfterRoute = function requestAfterRoute(server) {
-    // Fired after routing occurs
-};
-
-
-kraken.create(app).listen(function (err) {
-    if (err) {
-        console.error(err);
-    }
-});
-*/
