012cbf9055ca4114b308f7ea05a963ce1038ce0b
poornima <pvenkatakrishnan@paypal.com>
2014-03-14 18:54:42 -0700

    Adding support for dev tools as well

--- index.js
+++ index.js
@@ -6,16 +6,35 @@ var express = require('express'),
     kraken = require('kraken.next'),
     options = {
         onconfig: function (settings, cb) {
-            var config = {
+            var i18n = settings.get('i18n'),
+                specialization = settings.get('specialization'),
+                devtools,
+                engine,
+                dir;
+
+            //for dev-tools setup
+
+            if (i18n) {
+                devtools = settings.get('devtools');
+                devtools['arguments'][2].dust = {
+                    'dir': devtools['arguments'][2].dust,
+                    'i18n' : i18n
+                };
+                settings.set('devtools', devtools);
+            }
+
+            //for engine-munger setup
+
+            engine = {
                 'views': settings.get('express:views'),
                 'view engine': settings.get('express:view engine'),
-                'i18n': settings.get('i18n'),
-                'specialization': settings.get('specialization')
+                'i18n': i18n,
+                'specialization': specialization
             };
             var engines = settings.get('view engines');
 
-            engines.dust.renderer['arguments'].push(config);
-            engines.js.renderer['arguments'].push(config, app);
+            engines.dust.renderer['arguments'].push(engine);
+            engines.js.renderer['arguments'].push(engine, app);
 
             settings.set('view engines', engines);
             cb(null, settings);
